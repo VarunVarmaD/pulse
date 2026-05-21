017e3324271f030f1e8a617260edbbc6644bc61a
VarunVarmaD <varunvarmadk@gmail.com>
2026-05-21 16:49:28 +0530

    feat: set up express server skeleton

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Welcome');
+})
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
