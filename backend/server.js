955acabd7b9f45dffb2e7b8155746ea7a62458c4
VarunVarmaD <varunvarmadk@gmail.com>
2026-05-21 17:20:04 +0530

    feat: connect express to postgresql

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const pool = require('./db/pool.js');
+
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.get('/', (req, res) => {
     res.send('Welcome');
-})
+});
+
+(async () => {
+    try{
+        const result = await pool.query('SELECT NOW()', []);
+        console.log(result.rows);
+    }
+    catch(err){
+        console.log(err);
+    }
+})();
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 })
