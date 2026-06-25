6bd88c2a18498eea982cdf29b3528034078640f7
VarunVarmaD <varunvarmadk@gmail.com>
2026-06-25 11:32:00 +0530

    refactor: integrate docker orchestration and dynamic envs

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,12 +1,15 @@
 const pg = require('pg');
-const dotenv = require('dotenv');
-dotenv.config();
+
 const pool = new pg.Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    connectionString: process.env.DATABASE_URL,
+});
+
+pool.on('connect', () => {
+    console.log('Connected to the database');
+});
+
+pool.on('error', (err) => {
+    console.error('Error with the database', err);
 });
 
 module.exports = pool;
